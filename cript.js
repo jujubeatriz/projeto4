6bc5cb3d512a8af37d1f794b2766076f3717f036
jujubeatriz <110538881+jujubeatriz@users.noreply.github.com>
2024-07-04 20:55:42 -0300

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-reultado");
+const textoResultado = document.querySelector(".texto-resultado");
